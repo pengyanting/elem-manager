deed04a24f3ee4f761c09d449ecbae670ff14b76
pengyanting <18035072396@163.com>
2017-08-03 18:07:28 +0800

    init

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,95 @@
+var path = require('path')
+var utils = require('./utils')
+var config = require('../config')
+var vueLoaderConfig = require('./vue-loader.conf')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? config.build.assetsPublicPath
+      : config.dev.assetsPublicPath
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': resolve('src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [resolve('src'), resolve('test')],
+        options: {
+          formatter: require('eslint-friendly-formatter')
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderConfig
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test')]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ],
+    loaders: [
+      // 配置sass编译规则
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
+
+      },
+      {
+        // 让webpack去验证文件是否是.js结尾将其转换
+        test: /\.js$/,
+        // 通过babel转换
+        loader: 'babel',
+        // 不用转换的node_modules文件夹
+        exclude: /node_modules/,
+        query: {
+          'presets': ['es2015', 'stage-0'],
+          'plugins': ['transform-runtime']
+        }
+      },
+    ]
+  }
+}
---
+++ src/pages/index.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <header>
+      <el-row>
+        <el-col :span="12">
+          <div class="">
+            后台管理系统
+          </div>
+        </el-col>
+        <el-col :span="12">
+          <div class="right">
+            <img src="../assets/images/my-head.png" width='35' height='35' alt="">
+            <span>张三</span>
+            <span class='line'>|</span>
+            <span>系统管理员</span>
+            <el-dropdown @command="handleCommand">
+              <span class="el-dropdown-link">
+                <i class="el-icon-caret-bottom el-icon--right"></i>
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item command="a">首页</el-dropdown-item>
+                <el-dropdown-item command="b">退出</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </div>
+        </el-col>
+      </el-row>
+    </header>
+    <section>
+      <el-row>
+        <el-col :span='4'>
+          <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="Collapse">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-message"></i>
+                <span slot="title">用户</span>
+              </template>
+              <el-menu-item index="1-3">用户列表</el-menu-item>
+              <el-menu-item index="1-3">管理员列表</el-menu-item>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot='title'>
+                <i class="el-icon-menu"></i>
+                <span slot="title">商铺</span>
+              </template>
+              <el-menu-item index="1-3">商铺列表</el-menu-item>
+              <el-menu-item index="1-3">添加商铺</el-menu-item>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot='title'>
+                <i class="el-icon-menu"></i>
+                <span slot="title">食品</span>
+              </template>
+              <el-menu-item index="1-3">食品列表</el-menu-item>
+              <el-menu-item index="1-3">添加食品</el-menu-item>
+            </el-submenu>
+            <el-menu-item index="3">
+              <i class="el-icon-setting"></i>
+              <span slot="title">设置</span>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+        <el-col :span='20'></el-col>
+      </el-row>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      Collapse: ''
+    }
+  },
+  methods: {
+    handleCommand () {},
+    isCollapse () {},
+    handleOpen () {},
+    handleClose () {}
+  }
+}
+</script>
+<style lang='scss' scoped>
+@import '../assets/common.scss';
+header {
+  width: 100%;
+  height: 50px;
+  background: $color;
+  color: #fff;
+  line-height: 50px;
+  padding: 0 20px;
+  .right {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    img {
+      margin-right: 10px;
+    }
+    span {
+      font-size: 14px;
+    }
+    .line {
+      margin: 0 5px;
+    }
+  }
+  .el-icon-caret-bottom {
+    color: #fff;
+    margin-left: 5px;
+    cursor: pointer;
+  }
+}
+.el-menu-vertical-demo{
+  background: #FBFBFB;
+}
+</style>
